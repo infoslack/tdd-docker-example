61798f58fa977a4699e1989f81026b98245daf03
Daniel Romero <infoslack@gmail.com>
2015-03-02 16:59:31 -0300

    Add test for containers
    - Checking port mapping
    - Checking service availability

--- spec/Dockerfile_spec.rb
+++ spec/Dockerfile_spec.rb
@@ -5,11 +5,24 @@ describe "Dockerfile" do
     @image = Docker::Image.build_from_dir('.')
   }
 
-  it "image should exist" do
+  it "Image should exist" do
     expect(@image).to_not be_nil
   end
 
-  it "should expose the default tcp port" do
+  it "Should expose the default tcp port" do
     expect(@image.json["Config"]["ExposedPorts"]).to include("80/tcp")
+  end
+end
+
+describe "Running a container" do
+  
+  it "Should accept connections" do
+    id = `docker run -d -p 8080:80 infoslack/docker-nginx`.chomp
+    @container = Docker::Container.get(id)
+    
+    response = `curl -IL 127.0.0.1:8080`
+    expect(response).to include("HTTP/1.1 200 OK")
+
+    @container.kill
   end
 end
