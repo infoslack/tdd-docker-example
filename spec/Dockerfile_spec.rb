d97c70a89302769b4a2d5a8de37d3eb9b8f5a6b3
Daniel Romero <infoslack@gmail.com>
2015-03-02 16:20:15 -0300

    Add first test

--- spec/Dockerfile_spec.rb
+++ spec/Dockerfile_spec.rb
@@ -1,4 +1,15 @@
 require "docker"
 
 describe "Dockerfile" do
+  before(:all) {
+    @image = Docker::Image.build_from_dir('.')
+  }
+
+  it "image should exist" do
+    expect(@image).to_not be_nil
+  end
+
+  it "should expose the default tcp port" do
+    expect(@image.json["Config"]["ExposedPorts"]).to include("80/tcp")
+  end
 end
